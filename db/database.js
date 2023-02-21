346777067d026a5fa0defc202373c34a56a87dfa
TomBCold <TomBCold@gmail.com>
2023-02-21 09:17:00 +0300

    add timezone

--- db/database.js
+++ db/database.js
@@ -6,20 +6,23 @@ module.exports = {
     password: process.env.DB_PROJECT_PASSWORD,
     database: process.env.DB_PROJECT_DATABASE,
     host: process.env.DB_PROJECT_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+06:00'
   },
   test: {
     username: process.env.DB_PROJECT_USERNAME,
     password: process.env.DB_PROJECT_PASSWORD,
     database: process.env.DB_PROJECT_DATABASE,
     host: process.env.DB_PROJECT_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+06:00'
   },
   production: {
     username: process.env.DB_PROJECT_USERNAME,
     password: process.env.DB_PROJECT_PASSWORD,
     database: process.env.DB_PROJECT_DATABASE,
     host: process.env.DB_PROJECT_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+06:00'
   }
 };
